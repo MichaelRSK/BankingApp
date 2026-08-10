988cfa48d2107decfed420f986da9f9290397dec
Michael Krueger <michaelrsk2004@gmail.com>
2026-08-10 10:54:28 -0400

    Refactor Account class to implement deposit, withdraw, and balance retrieval methods

--- Account.py
+++ Account.py
@@ -1,9 +1,29 @@
-class Account:
-    def __init__(self):
-        self.amount = 0
+from abc import ABC, abstractmethod
 
-    def __init__(self, amount: int):
-        self.amount = amount
+class Account(ABC):
+    def __init__(self, owner: str, balance: int):
+        self.owner = owner
+        self._balance = balance # ENCAPSULATION
+
+    def deposit(self, amount):
+        if amount <= 0:
+            print("Deposit amount must be positive.")
+            return
+        self._balance += amount
+        print(f"Deposited ${amount}. New balance: ${self._balance:.2f}")
+    def withdraw(self, amount):
+        if amount > self._balance:
+            print("Insufficient funds.")
+            return
+        self._balance -= amount
+        print(f"Withdrew ${amount}. New balance: ${self._balance:.2f}")
+
+    def get_balance(self):
+        return self._balance
+
+    @abstractmethod
+    def account_type(self):
+        pass  # every subclass MUST implement this
 
 
 class SavingsAccount(Account):
