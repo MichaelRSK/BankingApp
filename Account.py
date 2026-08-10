5129dc72d8b846118e676b6ebb83acea2cd4e959
tectorm <tectorm@my.erau.edu>
2026-08-10 10:46:45 -0400

    basic structure

---
+++ Account.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self):
+        self.amount = 0
+
+    def __init__(self, amount: int):
+        self.amount = amount
+
+
+class SavingsAccount(Account):
+    pass
+
+class CheckingAccount(Account):
+    pass
+
+class User:
+    pass
