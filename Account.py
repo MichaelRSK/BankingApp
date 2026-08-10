4807d4791ec9424857d9aa85876537b61b63afb9
Qays Mukaddam <qaysmukaddam@gmail.com>
2026-08-10 11:21:51 -0400

    Adding comments

--- Account.py
+++ Account.py
@@ -5,16 +5,33 @@ class Account(ABC):
         self.owner = owner
         self._balance = balance # ENCAPSULATION
 
+
+    # Method used to deposit money into the account.
     def deposit(self, amount):
+        
+    # Check whether the amount being deposited is zero or negative.   
         if amount <= 0:
+            
+            
+     # Tell the user that the deposit amount must be positive.       
             print("Deposit amount must be positive.")
             return
+
+
         self._balance += amount
         print(f"Deposited ${amount}. New balance: ${self._balance:.2f}")
+    
+    
+     # Check whether the person is trying to withdraw more money
+     # than they currently have in the account.
     def withdraw(self, amount):
         if amount > self._balance:
+          
+     # Tell the user there isn't enough money.     
             print("Insufficient funds.")
             return
+        
+        
         self._balance -= amount
         print(f"Withdrew ${amount}. New balance: ${self._balance:.2f}")
 
